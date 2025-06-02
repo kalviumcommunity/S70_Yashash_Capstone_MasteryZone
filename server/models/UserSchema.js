fbc173a4522e08b6b37d6b97d57651c5eb8e9295
yashash1111 <yashash.r.s70@kalvium.community>
2025-06-02 21:28:45 +0530

    Implemented username/password authentication

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String }, // Optional if using Firebase Auth
   authProvider: { type: String, enum: ["email", "google"], default: "email" },
   avatarUrl: String,
   bio: String,
